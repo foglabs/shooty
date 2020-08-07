cb2a899d93ed049e7968a6d9f3a39e08cb02fb5b
foglabs <foglaboratories@gmail.com>
2020-08-07 15:39:22 -0400

    'and thats hwo the fuck you do that'

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ var app = express();
 var cors = require('cors');
 
 app.use(cors());
-app.use(express.static('sprites'));
+app.use(express.static('app'));
 const path = require('path');
 
 
 // main site
 app.get('/', function(req, res, next) {
   res.header('Content-Type', 'text/html');
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
 
 app.listen(4000);
